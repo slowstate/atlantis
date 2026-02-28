25b6c56d4e0b9aa8d6ba08e357d2f43e548c0c5c
slowstate <slowstatedev@gmail.com>
2026-02-28 22:56:31 +1100

    WIP composing shallows, updated movement

--- atlantis/argo/argo.gd
+++ atlantis/argo/argo.gd
@@ -7,6 +7,14 @@ var argo_acceleration := 50
 var argo_max_speed := 100
 var argo_friction := 40
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_body_1: CollisionShape2D = $CollisionShapeBody1
+@onready var collision_shape_body_2: CollisionShape2D = $CollisionShapeBody2
+@onready var collision_shape_body_3: CollisionShape2D = $CollisionShapeBody3
+@onready var collision_shape_top: CollisionShape2D = $CollisionShapeTop
+@onready var collision_shape_body_4: CollisionShape2D = $CollisionShapeBody4
+@onready var collision_shape_body_5: CollisionShape2D = $CollisionShapeBody5
+
 
 func _ready() -> void:
 	Globals.argo = self
@@ -22,10 +30,12 @@ func _process(delta: float) -> void:
 			move_vec.y = -1
 		if Input.is_action_pressed("player_move_left"):
 			move_vec.x = -1
+			sprite_2d.flip_h = false
 		if Input.is_action_pressed("player_move_down"):
 			move_vec.y = 1
 		if Input.is_action_pressed("player_move_right"):
 			move_vec.x = 1
+			sprite_2d.flip_h = true
 
 	## ARGO: Move "realistic" damping, diagonal max speed higher than single axis, more floaty
 	if move_vec.x != 0:
